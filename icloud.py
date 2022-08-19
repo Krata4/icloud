cc4b0fd60e2ee663b4c894311acc8cbcd784c02a
Krata4 <70528301+Krata4@users.noreply.github.com>
2022-08-19 15:35:36 +0200

    Update icloud.py

--- icloud.py
+++ icloud.py
@@ -2,20 +2,24 @@ from pyicloud import PyiCloudService
 import asyncio
 import sys, getopt
 
-username = "yourusername"
+username = "xourusername"
 
 async def icloud():
     api = PyiCloudService(username)
     opts, args = getopt.getopt(sys.argv[1:],"dl:s:p:",["devices","localization=","status=","playsound="])
+    print(f'{{')
     for opt, arg in opts:
         if opt in ("-d", "--devices"):
+            print("devices:")
             print(api.devices)
         elif opt in ("-l", "--localization"):
+            print("localization:")
             print(api.devices[arg].location())
         elif opt in ("-s", "--status"):
+            print("status:")
             print(api.devices[arg].status())
-            #print(arg)
         elif opt in ("-p", "--playsound"):
             print(api.devices[arg].play_sound())
+    print(f"}}")
 
 asyncio.run(icloud())
