11f11af8fd787dc71be707c06feace60ee6501c9
krata4 <jirkratochvil@gmail.com>
2022-08-19 22:54:30 +0200

    direct update via REST

--- icloud.py
+++ icloud.py
@@ -1,12 +1,16 @@
 from pyicloud import PyiCloudService
 import asyncio
 import sys, getopt
+import requests
 
 username = "yourusername"
+item_name = "yoouritemname"
 
 async def icloud():
     api = PyiCloudService(username)
-    opts, args = getopt.getopt(sys.argv[1:],"dl:s:p:",["devices","localization=","status=","playsound="])
+    opts, args = getopt.getopt(sys.argv[1:],"dul:s:p:",["devices","upload","localization=","status=","playsound="])
+    upload = False
+    loc = ""
     print(f'{{')
     for opt, arg in opts:
         if opt in ("-d", "--devices"):
@@ -15,7 +19,8 @@ async def icloud():
             print(",")
         elif opt in ("-l", "--localization"):
             print("localization:")
-            print(api.devices[arg].location())
+            loc = api.devices[arg].location()
+            print(loc)
             print(",")
         elif opt in ("-s", "--status"):
             print("status:")
@@ -23,6 +28,10 @@ async def icloud():
             print(",")
         elif opt in ("-p", "--playsound"):
             print(api.devices[arg].play_sound())
+        elif opt in ("-u", "--upload"):
+            upload = True
     print(f"}}")
-
+    if upload:
+        loc_gps = str(loc['latitude']) + "," + str(loc['longitude'])
+        requests.put('http://openhab:8080/rest/items/'+item_name+'/state', str(loc_gps), headers={'Content-type': 'text/plain'})
 asyncio.run(icloud())
