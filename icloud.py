a6bba352980413acb82d01f9359b27ed4772ce6f
Krata4 <70528301+Krata4@users.noreply.github.com>
2022-08-19 14:26:21 +0200

    Add files via upload

---
+++ icloud.py
@@ -0,0 +1,20 @@
+from pyicloud import PyiCloudService
+import asyncio
+import sys, getopt
+
+username = "yourusername"
+
+async def icloud():
+    api = PyiCloudService(username)
+    opts, args = getopt.getopt(sys.argv[1:],"dlsp:",["devices","localization=","status=","play_sound="])
+    for opt, arg in opts:
+        if opt in ("-d", "--devices"):
+            print(api.devices)
+        elif opt in ("-l", "--localization"):
+            print(api.devices[arg].location())
+        elif opt in ("-s", "--status"):
+            print(api.devices[arg].status())
+        elif opt in ("-p", "--play_sound"):
+            print(api.devices[arg].play_sound())
+
+asyncio.run(icloud())
